fdb7b2c3e4c5b4501756fc0e0f35ad6e95e1f383
real_asgari@yahoo.com <real_asgari@yahoo.com>
2020-06-06 02:58:13 +0430

    14 - Bootstrap & Django Templates are added

--- social_network/urls.py
+++ social_network/urls.py
@@ -20,6 +20,6 @@ from tweets.views import home_view, tweet_detail_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
-    path('tweets/<int:tweet_id>', home_view),
+    path('tweets/', home_view),
     path('tweets/<int:tweet_id>', tweet_detail_view)
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
+from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse("<h1>hi hi</h1>")
+    # print(args, kwargs)
+    return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse(f"<h1>{tweet_id}</h1>")
+    """
+    :param request:
+    :param tweet_id:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift or Java or ios/Android
+    return json data
+    """
+
+    # print(args, kwargs)
+    data = {
+        "id": tweet_id,
+        # "content": obj.content,
+        # "image_path":obj.image.url
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not Found"
+        status = 404
+
+        # raise Http404
+
+    # return HttpResponse(f"<h1> {tweet_id} - {obj.content} </h1>")
+    return JsonResponse(data, status=status)  # json.dumps content_type ='application/json'
+
