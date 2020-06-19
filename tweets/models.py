73c5e6491b502916eb0d1fa6eab2bd36117fa599
real_asgari@yahoo.com <real_asgari@yahoo.com>
2020-06-19 23:58:50 +0430

    33 - Users & Tweets are added

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 import random
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
     # Maps to SQL data
     # id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # many users can have many tweets , one tweet can just have one User its why we use ForeinKey itself
+    #ForeingKey -> creating the relationships between these two tables
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
